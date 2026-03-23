317407b57f92cfe19120eb752f13233fa433c3ff
Pug <iakingdoms@gmail.com>
2026-03-23 21:11:18 +1100

    creationts update

--- src/worldgen/block.ts
+++ src/worldgen/block.ts
@@ -3,6 +3,19 @@ import {BufferGeometryUtils} from "three/examples/jsm/Addons.js";
 import {CuboidMesh, CuboidMeshMultiTexture, CuboidMeshOneColor, CuboidMeshOneTexture} from "../creation";
 
 export class Block {
+    static BlockMeshes = new Map<string, CuboidMesh>([
+        ["grass", new CuboidMeshMultiTexture(1, 1, 1, 1.0, 
+            "grass_textures/grass_top.png",
+            "grass_textures/grass_bottom.png",
+            "grass_textures/grass_side.png",
+        )],
+        ["dirt", new CuboidMeshOneTexture(1, 1, 1, 1.0, "one_texture/dirt.png")],
+        ["stone", new CuboidMeshOneTexture(1, 1, 1, 1.0, "one_texture/stone.png")],
+        ["glass", new CuboidMeshOneTexture(1, 1, 1, 1.0, "one_texture/glass.png")],
+        ["coal", new CuboidMeshOneTexture(1, 1, 1, 1.0, "one_texture/coal.png")],
+        ["iron", new CuboidMeshOneTexture(1, 1, 1, 1.0, "one_texture/iron.png")]
+    ]);
+
     name: string;
     opacity: number;
     color: THREE.Color | null;
@@ -15,44 +28,6 @@ export class Block {
 
     getMesh(faces: {px: number; nx: number; py: number; ny: number; pz: number; nz: number}): THREE.Mesh | null {
         if (this.opacity === 0 || this.color == null) return null;
-        let mesh: CuboidMesh;
-
-        switch(this.name) {
-            case "grass": {
-                mesh = new CuboidMeshMultiTexture(1, 1, 1, 1.0, 
-                    "grass_textures/grass_top.png",
-                    "grass_textures/grass_bottom.png",
-                    "grass_textures/grass_side.png",
-                    faces,
-                );
-                break;
-            }
-            case "dirt": {
-                mesh = new CuboidMeshOneTexture(1, 1, 1, 1.0, "one_texture/dirt.png", faces);
-                break;
-            }
-            case "stone": {
-                mesh = new CuboidMeshOneTexture(1, 1, 1, 1.0, "one_texture/stone.png", faces);
-                break;
-            }
-            case "glass": {
-                mesh = new CuboidMeshOneTexture(1, 1, 1, 1.0, "one_texture/glass.png", faces);
-                break;
-            }
-            case "coal": {
-                mesh = new CuboidMeshOneTexture(1, 1, 1, 1.0, "one_texture/coal.png", faces)
-                break;
-            }
-            case "iron": {
-                mesh = new CuboidMeshOneTexture(1, 1, 1, 1.0, "one_texture/iron.png", faces);
-                break;
-            }
-            default: {
-                mesh = new CuboidMeshOneColor(1, 1, 1, 1.0, this.color, false, faces);
-                break;
-            }
-        }
-
-        return mesh.Mesh();
+        return Block.BlockMeshes.get(this.name)?.Mesh(faces) ?? null;
     }
 }
