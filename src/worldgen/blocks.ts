ae25f59c63f2f3d48b633e30627cd16b3e3a5183
Samcask <sam.caskey11@gmail.com>
2026-03-25 16:28:59 +1100

    New grass/dirt textures

--- src/worldgen/blocks.ts
+++ src/worldgen/blocks.ts
@@ -6,19 +6,19 @@ export const AIR = new Block("air", false, true,
     null);
 export const GRASS = new Block("grass", true, false,
     new CuboidMeshMultiTexture(1, 1, 1, 1.0,
-        "grass_textures/grass_top.png",
-        "grass_textures/grass_bottom.png",
-        "grass_textures/grass_side.png",
+        "textures/grass_top.png",
+        "textures/grass_bottom.png",
+        "textures/grass_side.png",
     ));
 export const DIRT = new Block("dirt", true, false,
-    new CuboidMeshOneTexture(1, 1, 1, 1.0, "one_texture/dirt.png"));
+    new CuboidMeshOneTexture(1, 1, 1, 1.0, "textures/dirt.png"));
 export const STONE = new Block("stone", true, false,
-    new CuboidMeshOneTexture(1, 1, 1, 1.0, "one_texture/stone.png"));
+    new CuboidMeshOneTexture(1, 1, 1, 1.0, "textures/stone.png"));
 export const GLASS = new Block("glass", true, true,
-    new CuboidMeshOneTexture(1, 1, 1, 1.0, "one_texture/glass.png"));
+    new CuboidMeshOneTexture(1, 1, 1, 1.0, "textures/glass.png"));
 export const COAL = new Block("coal", true, false,
-    new CuboidMeshOneTexture(1, 1, 1, 1.0, "one_texture/coal.png"));
+    new CuboidMeshOneTexture(1, 1, 1, 1.0, "textures/coal.png"));
 export const IRON = new Block("iron", true, false,
-    new CuboidMeshOneTexture(1, 1, 1, 1.0, "one_texture/iron.png"));
+    new CuboidMeshOneTexture(1, 1, 1, 1.0, "textures/iron.png"));
 export const CUCUMBER = new Block("cucumber", true, false,
     new CuboidMeshOneColor(1, 1, 1, 1, new Color(0.1, 0.5, 0.2), false));
