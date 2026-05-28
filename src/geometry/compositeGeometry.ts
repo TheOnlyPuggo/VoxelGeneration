68538cecdc4bd606a854f92fb77f0616823b25f6
Samcask <sam.caskey11@gmail.com>
2026-05-28 18:19:07 +1000

    overload for adding single geometry to composite

--- src/geometry/compositeGeometry.ts
+++ src/geometry/compositeGeometry.ts
@@ -23,11 +23,29 @@ export class CompositeGeometry {
         }
     }
 
-    addGeometries(geometries: BufferGeometry[], materials: Material[]): void {
-        for (let i = 0; i < Math.min(geometries.length, materials.length); i++) {
-            const geometriesWithMaterial: BufferGeometry[] | undefined = this.geometries.get(materials[i]);
-            if (geometriesWithMaterial) geometriesWithMaterial.push(geometries[i]);
-            else this.geometries.set(materials[i], [geometries[i]]);
+    addGeometries(geometry: BufferGeometry | BufferGeometry[], material: Material | Material[]): void {
+        if (geometry instanceof Array) {
+            if (material instanceof Array) {
+                for (let i = 0; i < Math.min(geometry.length, material.length); i++) {
+                    const geometriesWithMaterial: BufferGeometry[] | undefined = this.geometries.get(material[i]);
+                    if (geometriesWithMaterial) geometriesWithMaterial.push(geometry[i]);
+                    else this.geometries.set(material[i], [geometry[i]]);
+                }
+            } else {
+                let geometriesWithMaterial: BufferGeometry[] | undefined = this.geometries.get(material);
+                if (!geometriesWithMaterial) {
+                    geometriesWithMaterial = [];
+                    this.geometries.set(material, geometriesWithMaterial);
+                }
+                for (let i = 0; i < geometry.length; i++) geometriesWithMaterial.push(geometry[i]);
+            }
+        } else {
+            if (material instanceof Array) {
+                material = material[0];
+            }
+            const geometriesWithMaterial: BufferGeometry[] | undefined = this.geometries.get(material);
+            if (geometriesWithMaterial) geometriesWithMaterial.push(geometry);
+            else this.geometries.set(material, [geometry]);
         }
     }
 
